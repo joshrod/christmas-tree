2b284df2c3b2a3e63c132ed8d46e814db44f6bad
Joshua Rodriguez <j_rodriguez@m110.local>
2018-12-04 19:22:12 -0600

    Mobile portrait mode complete functionality of christmas grid. Lightbox and grid work in both portrait and landscape.

--- js/script.js
+++ js/script.js
@@ -2,18 +2,55 @@ const lightbox = document.querySelector('.lightbox');
 const cards = document.querySelectorAll('.card');
 const lbImages = document.querySelectorAll('.img-container');
 const snippets = document.querySelectorAll('.snippet');
+const close = document.querySelector('.lb-close');
+
+let lbOpen = false;
+let current = null;
 
 
 window.onload = () => {
+    for (let i = 0; i < lbImages.length; i++) {
+        const image = lbImages[i];
+        const snippet = snippets[i];
+        image.style.display = 'none';
+        snippet.style.display = 'none';
+    }
+
     for (let i = 0; i < cards.length; i++) {
-        cards[i].onclick = () => openLightbox();
+        cards[i].onclick = () => openLightbox(i);
+    }
+
+    close.onclick = () => {
+        closeLightbox();
     }
 }
 
-function openLightbox() {
-    lightbox.classList.remove('is-hidden');
-    lightbox.classList.add('is-visible');
+function openLightbox(index) {
+    if (lbOpen) {
+        return;
+    }
+    lbOpen = true;
+    current = index;
+    let tl = new TimelineMax();
+    tl.to(cards[index], 0.2, {y: -20, ease:Power2.easeOut}).to(cards[index], 0.3, {y: 0, ease: Bounce.easeOut, onComplete: function() {
+        lightbox.classList.remove('is-hidden');
+        lightbox.classList.add('is-visible');
+        lbImages[index].style.display = 'block';
+        snippets[index].style.display = 'block';
+        close.style.display = 'block';
+    }})
+    .addLabel('lbAnimation')
+    .from(lbImages[index], 0.6, {yPercent: -150, opacity: 0, delay: 0.2}, 'lbAnimation')
+    .from(snippets[index], 0.6, {yPercent: 300, opacity: 0, delay: 0.2}, 'lbAnimation')
+    .from(close, 0.3, {opacity: 0, scale: 0.5});
+}
 
-    TweenMax.from(lbImages[0], 0.8, {yPercent: -150, opacity: 0, delay: 0.2});
-    TweenMax.from(snippets[0], 0.8, {yPercent: 300, opacity: 0, delay: 0.2});
+function closeLightbox() {
+    lightbox.classList.remove('is-visible');
+    lightbox.classList.add('is-hidden');
+    lbImages[current].style.display = 'none';
+    snippets[current].style.display = 'none';
+    close.style.display = 'none';
+    current = null;
+    lbOpen = false;
 }
